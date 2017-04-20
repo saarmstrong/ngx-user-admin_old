f5739bae1415b74d8faa126708cc46070adac77b
Andy Armstrong <andyarmstro@gmail.com>
2017-04-19 17:26:25 -0700

    more tweaks ; added ngx-bootstrap

--- src/user-admin.module.ts
+++ src/user-admin.module.ts
@@ -3,14 +3,29 @@ import { NgModule }             from '@angular/core';
 import { BrowserModule }            from '@angular/platform-browser';
 import { HttpModule }               from '@angular/http';
 
+import { RouterModule, Routes }     from '@angular/router';
+
+import { AlertModule,
+         ModalModule,
+         TabsModule }               from 'ngx-bootstrap';
+
 import { AuthService }              from './auth.service';
 import { UsersService }             from './users.service';
 
 import { UserListComponent }    from './user-list.component';
 
+const loginRoutes: Routes = [
+];
+
 @NgModule({
     imports: [ 
+        BrowserModule,
+        HttpModule,
+        RouterModule.forRoot(loginRoutes),
 
+        AlertModule,
+        ModalModule,
+        TabsModule
     ],
     declarations: [ 
         UserListComponent 
